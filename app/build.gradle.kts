fa24835aff887c97d621c3ac7c40ddda6f5adb87
Jean Newman <newmanrios@gmail.com>
2024-04-12 14:11:30 -0400

    permission managament

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ android {
 dependencies {
 
     val navVersion = "2.7.7"
+    val camVersion = "1.2.3"
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -75,6 +76,14 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+    //camera x
+    implementation("androidx.camera:camera-camera2:$camVersion")
+    implementation("androidx.camera:camera-lifecycle:$camVersion")
+    implementation("androidx.camera:camera-view:$camVersion")
+    implementation("androidx.camera:camera-core:$camVersion")
+    implementation("androidx.camera:camera-extensions:$camVersion")
+
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/jeandarwinnewmanrios/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/jeandarwinnewmanrios/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -5,17 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import com.jeandarwinnewmanrios.horoscapp.R
 import com.jeandarwinnewmanrios.horoscapp.databinding.FragmentLuckBinding
 import com.jeandarwinnewmanrios.horoscapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.security.Permission
 
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
+    companion object {
+        private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
+    }
     private var _binding: FragmentPalmistryBinding? = null //que siginifica _binding y binding en este caso?  //binding es una propiedad privada que devuelve el valor de _binding y _binding es una variable privada que es nula
     private val binding get() = _binding!!
 
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permission is granted. Continue the action or workflow in your app.
+        } else {
+            // Explain to the user that the feature is unavailable because the
+            // features requires a permission that the user has denied. At the
+            // same time, respect the user's decision. Don't link to system
+            // settings in an effort to convince the user to change their
+            // decision.
+            Toast.makeText(
+                requireContext(),
+                "Access to the camera is required to take a picture of your palm. Please enable the camera permission in the app settings.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (checkCameraPermission()) {
+
+        }
+        else{
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
+        }
+    }
+
+    private fun checkCameraPermission(): Boolean {
+      return PermissionChecker.checkPermission(
+            requireContext(),
+            CAMERA_PERMISSION,
+            android.os.Process.myPid(),
+            android.os.Process.myUid(),
+            requireActivity().packageName
+      ) == PermissionChecker.PERMISSION_GRANTED
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
